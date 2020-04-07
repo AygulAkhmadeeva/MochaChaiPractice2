548481b5062ad17045bec10874f1fc8e42af5a95
AygulAkhmadeeva <aygulaaa@gmail.com>
2020-04-06 23:42:10 -0700

    chai/expect

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,7 +10,7 @@ describe ('function sum', () => {
   });
 
   it('should sum not equals 7', () => {
-    expect(sum(2, 4)).to.not.equal(7);
+    expect(sum(2, 4)).to.not.eq(7);
   });
 });
 
@@ -65,67 +65,67 @@ describe ('function spendEntireGiftCard', () => {
 describe ('function greet', () => {
 
   it('should return Welcome when language is English', ()=>{
-    expect(greet('english')).equal('Welcome');
+    expect(greet('english')).eq('Welcome');
   });
   it('should return correct greeting if language is not English and is in database', ()=>{
-    expect(greet('estonian')).equal('Tere tulemast');
+    expect(greet('estonian')).eql('Tere tulemast');
   });
   it('should return Welcome if language is not in database', ()=>{
-    expect(greet('swedish')).equal('Welcome');
+    expect(greet('swedish')).eq('Welcome');
   });
   it('should return Welcome if input has not argument', ()=>{
-    expect(greet()).equal('Welcome');
+    expect(greet()).eq('Welcome');
   });
   it('should return Welcome if input is a number', ()=>{
-    expect(greet(5)).equal('Welcome');
+    expect(greet(5)).eq('Welcome');
   });
   it('should return Welcome if input is an array', ()=>{
-    expect(greet([])).equal('Welcome');
+    expect(greet([])).eq('Welcome');
   });
 });
 
 describe ('function cubeOdd', () => {
 
   it('should return correct result', ()=>{
-    expect(cubeOdd([1, 2, 3, 4])).equal(28);
+    expect(cubeOdd([1, 2, 3, 4])).eq(28);
   });
   it('should return undefined if not all numbers in the list', ()=>{
     //assert.equal(cubeOdd(["a",12,9,"z",42]), undefined);
     expect(cubeOdd(['a',12,9,'z',42])).undefined;
   });
   it('should return 0 if there is no odd numbers in the list', ()=>{
-    expect(cubeOdd([6, 2, 8, 4])).equal(0);
+    expect(cubeOdd([6, 2, 8, 4])).eq(0);
   });
   it('should return 0 if list is empty', ()=>{
-    expect(cubeOdd([])).equal(0);
+    expect(cubeOdd([])).eq(0);
   });
   it('should return undefined if input is a string', ()=>{
     assert.equal(cubeOdd('zzz'), undefined);
     expect(cubeOdd('zzz')).undefined;
   });
   it('should return 0 if input is an object', ()=>{
-    expect(cubeOdd({})).equal(0);
+    expect(cubeOdd({})).eq(0);
   });
   it('should return 0 if input is a number', ()=>{
-    expect(cubeOdd(7)).equal(0);
+    expect(cubeOdd(7)).eq(0);
   });
   it('should return 0 if input has no argument', ()=>{
-    expect(cubeOdd()).equal(0);
+    expect(cubeOdd()).eq(0);
   });
 });
 
 describe ('function noSpace', () => {
   it('should return correct result if there is single space', ()=>{
-    expect(noSpace('8 j 8 mBli\n\rB8g imjB8B8 jl B')).equal('8j8mBliB8gimjB8B8jlB');
+    expect(noSpace('8 j 8 mBli\n\rB8g imjB8B8 jl B')).eq('8j8mBliB8gimjB8B8jlB');
   });
   it('should return correct result if there are multiple spaces', ()=>{
-    expect(noSpace('8   8 Bi fk8h  B 8 BB8B')).equal('88Bifk8hB8BB8B');
+    expect(noSpace('8   8 Bi fk8h  B 8 BB8B')).eq('88Bifk8hB8BB8B');
   });
   it('should return correct result if there are spaces at the beginning and at the end', ()=>{
-    expect(noSpace('  8aaaaa dddd r   ')).equal('8aaaaaddddr');
+    expect(noSpace('  8aaaaa dddd r   ')).eq('8aaaaaddddr');
   });
   it('should return correct result if there are special characters /-$,./', ()=>{
-    expect(noSpace('djk-13 - hf  3 $..  hy9 , y$ ')).equal('djk-13-hf3$..hy9,y$');
+    expect(noSpace('djk-13 - hf  3 $..  hy9 , y$ ')).eq('djk-13-hf3$..hy9,y$');
   });
   it('should return "wrong input" if input is a number', ()=>{
     expect(noSpace(5)).equal('wrong input');
@@ -150,7 +150,7 @@ describe ('function times100', () => {
     expect(times100('aya').every(e => e === 'aya')).equal(true);
   });
   it('should contain the word set in variable', ()=>{
-    expect(times100('aya')).to.include('anna');
+    expect(times100('aya')).to.include('aya');
     expect(times100('aya')).to.be.an('array').that.includes('aya');
   });
   it('should return the array of numbers if input is a number', ()=>{
@@ -166,111 +166,111 @@ describe ('function times100', () => {
 
 describe ('function digital_root', () => {
   it('should return correct result on a short input value', ()=>{
-    expect(digital_root(16)).equal(7);
+    expect(digital_root(16)).eq(7);
   });
   it('should return correct result on a long input value', ()=>{
-    expect(digital_root(456)).equal(6);
+    expect(digital_root(456)).eq(6);
   });
   it('should return correct result input is negative', ()=>{
-    expect(digital_root(-56)).equal(2);
+    expect(digital_root(-56)).eq(2);
   });
   it('should return correct result input is 0', ()=>{
-    expect(digital_root(0)).equal(0);
+    expect(digital_root(0)).eq(0);
   });
   it('should return 0 with no argument', ()=>{
-    expect(digital_root()).equal(0);
+    expect(digital_root()).eq(0);
   });
   it('should return "unexpected input" if input is a string', ()=>{
-    expect(digital_root('zzz')).equal('unexpected input');
+    expect(digital_root('zzz')).eq('unexpected input');
   });
   it('should return "unexpected input" if input is an array', ()=>{
-    expect(digital_root([2, 7])).equal('unexpected input');
+    expect(digital_root([2, 7])).eq('unexpected input');
   });
 });
 
 describe ('function inAscOrder', () => {
   it('should return true if elements are in ascending order', ()=>{
-    expect(inAscOrder([1, 2, 4, 7, 19])).to.be.true;
+    expect(inAscOrder([1, 2, 4, 7, 19])).true;
   });
   it('should return false if elements are in arbitrary order', ()=>{
-    expect(inAscOrder([1, 6, 10, 18, 2, 4, 20])).to.be.false;
+    expect(inAscOrder([1, 6, 10, 18, 2, 4, 20])).false;
   });
   it('should return false if elements are in descending order', ()=> {
-    expect(inAscOrder([9,8,7,6,5,4,3,2,1])).to.be.false;
+    expect(inAscOrder([9,8,7,6,5,4,3,2,1])).false;
   });
   it('should return true if there are negative elements in ascending order', ()=>{
-    expect(inAscOrder([-2, -1, 0, 4, 7, 19])).to.be.true;
+    expect(inAscOrder([-2, -1, 0, 4, 7, 19])).true;
   });
   it('should return false if there are negative elements in arbitrary order', ()=>{
-    expect(inAscOrder([-1, -2, 3, 4, 6, 19])).to.be.false;
+    expect(inAscOrder([-1, -2, 3, 4, 6, 19])).false;
   });
   it('should return null on array with 1 element', ()=>{
-    expect(inAscOrder([1])).to.be.true;
+    expect(inAscOrder([1])).true;
   });
   it('should return true on an empty list', ()=>{
-    expect(inAscOrder([])).to.be.true;
+    expect(inAscOrder([])).true;
   });
   it('should return true with no arguments', ()=>{
-    expect(inAscOrder()).to.be.true;
+    expect(inAscOrder()).true;
   });
   it('should return "Wrong input" if input is a number', ()=>{
-    expect(inAscOrder(6)).equal('Wrong input');
+    expect(inAscOrder(6)).eq('Wrong input');
   });
   it('should return "Wrong input" if input is a string', ()=>{
-    expect(inAscOrder('zzz')).equal('Wrong input');
+    expect(inAscOrder('zzz')).eq('Wrong input');
   });
 });
 
 describe ('function initializeNames', () => {
   it('should return correct result if name has only first name and last name', ()=>{
-    expect(initializeNames('Anna Strik')).equal('Anna Strik');
+    expect(initializeNames('Ana Le')).eq('Ana Le');
   });
   it('should return correct result if name has only first name', ()=>{
-    expect(initializeNames('Fedor')).equal('Fedor');
+    expect(initializeNames('Fedor')).eq('Fedor');
   });
   it('should return correct result if name has 3 names', ()=>{
-    expect(initializeNames('Lois Mary Lane')).equal('Lois M. Lane');
+    expect(initializeNames('Lois Mary Lane')).eq('Lois M. Lane');
   });
   it('should return correct result if name has 6 names', ()=>{
-    expect(initializeNames('Alice Betty Catherine Jane Anna Davis')).equal('Alice B. C. J. A. Davis');
+    expect(initializeNames('Alice Betty Catherine Jane Anna Davis')).eq('Alice B. C. J. A. Davis');
   });
   it('should return empty string with no argument', ()=>{
-    expect(initializeNames()).equal('');
+    expect(initializeNames()).eq('');
   });
   it('should return empty string with input empty string', ()=>{
-    expect(initializeNames('')).equal('');
+    expect(initializeNames('')).eq('');
   });
   it('should return "wrong format" if input is a number', ()=>{
-    expect(initializeNames(4)).equal('wrong format');
+    expect(initializeNames(4)).eq('wrong format');
   });
   it('should return "wrong format" if input is an array', ()=>{
-    expect(initializeNames([1, 2])).equal('wrong format');
+    expect(initializeNames([1, 2])).eq('wrong format');
   });
 });
 
 describe ('function array_diff', () => {
   it('should return correct result if there are no duplicated elements in each array', ()=>{
-    expect(array_diff([1, 2, 3],[1])).deep.equal([2, 3]);
+    expect(array_diff([1, 2, 3],[1])).deep.eq([2, 3]);
   });
   it('should return correct result if there are duplicated elements in each array', ()=>{
-    expect(array_diff([1,2,2,2,3,4],[4,2,4])).deep.equal([1, 3]);
+    expect(array_diff([1,2,2,2,3,4],[4,2,4])).deep.eq([1, 3]);
   });
   it('should return correct result if there are negative elements in arrays', ()=>{
     expect(array_diff([12,0,-18,8,-6,-13,-4,-6,-3],[-3,-6,-4,0,12,-6,-18,8])).deep.equal([-13]);
   });
   it('should return correct result if second element is empty', ()=>{
-    expect(array_diff([1,8,2], [])).deep.equal([1,8,2]);
+    expect(array_diff([1,8,2], [])).deep.eq([1,8,2]);
   });
   it('should return empty array if first element is empty', ()=>{
     expect(array_diff([], [1, 2, 3])).to.be.an('array').that.is.empty;
   });
   it('should return "wrong data format" with no arguments', ()=>{
-    expect(array_diff()).equal('wrong data format');
+    expect(array_diff()).eq('wrong data format');
   });
   it('should return "wrong data format" if input is a number', ()=>{
-    expect(array_diff(6)).equal('wrong data format');
+    expect(array_diff(6)).eq('wrong data format');
   });
   it('should return "wrong data format" if input is a string', ()=>{
-    expect(array_diff('zzz')).equal('wrong data format');
+    expect(array_diff('zzz')).eq('wrong data format');
   });
 });
